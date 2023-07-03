83814230d7df3a9a13e435618e973a8973026508
nabil-mabrouk <nabil.mabrouk.ai@gmail.com>
2023-07-03 23:17:03 +0200

    update chapter 2

---
+++ chapter2-understanding GPT/src/ch2.py
@@ -0,0 +1,15 @@
+# code source for chapter 2
+
+# Import necessary modules
+import os
+import sys
+
+# Define main function
+def main():
+    # Your application logic here
+    print("Hello, world!")
+
+# Check if the script is being run directly
+if __name__ == "__main__":
+    # Call the main function
+    main()
